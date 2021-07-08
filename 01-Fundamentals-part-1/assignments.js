9f9c4efb21fbe2fc0f350aac89082fcfe8669ca8
Ivan-Andr <levchenko-ivan@mail.ru>
2021-07-08 14:38:46 +0500

    deleted one assignment.js

--- 01-Fundamentals-part-1/assignments.js
+++ 01-Fundamentals-part-1/assignments.js
@@ -1,1 +1,5 @@
 console.log(`This is "assignments.js" file`)
+let country = 'Uzbekistan';
+let continent = 'Asia';
+let population = 35000000;
+console.log("Population of", country, 'is', population, 'located in', continent);
--- assignment.js
+++
@@ -1,4 +0,0 @@
-let country = 'Uzbekistan';
-let continent = 'Asia';
-let population = 35000000;
-console.log("Population of", country, 'is', population, 'located in', continent);
