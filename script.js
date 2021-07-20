53f5892d21e27203c82a1a7beb52845d713ec5f9
Ivan-Andr <levchenko-ivan@mail.ru>
2021-07-20 21:48:25 +0500

    complete consts and template literals

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log(`This is "script.js" file`);
+/*console.log(`This is "script.js" file`);
 let js = "JS is amazing!";
 console.log(40 + 48 - 11 * 8);
 console.log('Ivan');
@@ -10,4 +10,15 @@ firtstName = 'Vano';
 console.log(firtstName);
 let _name = "Sergey";
 let $name = 'ivan';
-console.log(_name, $name);
+console.log(_name, $name);*/
+
+const firtstName = "Ivan";
+const job = "engeneer";
+const birthYear = 1987;
+const year = 2021;
+const Ivan =
+  "I'm " + firtstName + ",a " + (year - birthYear) + " years old " + job + "!";
+console.log(Ivan);
+const ivanNew = `I'm ${firtstName},a ${birthYear} years old ${job}!`;
+console.log(ivanNew);
+console.log(ivanNew);
