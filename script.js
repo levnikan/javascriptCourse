3e1c32cdbd7be353e74956adac7114875ee34686
Ivan-Andr <levchenko-ivan@mail.ru>
2021-07-22 22:13:41 +0500

    add new var

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 console.log(`This is "script.js" file`);
 let js = "JS is amazing!";
 console.log(40 + 48 - 11 * 8);
-console.log('Ivan');
-let firtstName = 'Ivan';
+console.log("Ivan");
+let firtstName = "Ivan";
 let age = 34;
 console.log(firtstName);
 console.log(age);
-firtstName = 'Vano';
+firtstName = "Vano";
 console.log(firtstName);
 let _name = "Sergey";
-let $name = 'ivan';
+let $name = "ivan";
 console.log(_name, $name);
+
+const weigtMark = 78;
