a881073d716665b6fc7560bf584309f343877b35
Ivan-Andr <levchenko-ivan@mail.ru>
2021-08-15 21:17:46 +0500

    if/else statement

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let _name = "Sergey";
 let $name = 'ivan';
 console.log(_name, $name);*/
 
-const firtstName = "Ivan";
+/*const firtstName = "Ivan";
 const job = "engeneer";
 const birthYear = 1987;
 const year = 2021;
@@ -21,4 +21,23 @@ const Ivan =
 console.log(Ivan);
 const ivanNew = `I'm ${firtstName},a ${birthYear} years old ${job}!`;
 console.log(ivanNew);
-console.log(ivanNew);
+console.log(ivanNew);*/
+const age=17;
+//const isOldEnough=age>=18;
+if(age>=18) {
+  console.log('Sarah can start driving license');
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Saarah is too young. Wait for ${yearsLeft} years!`);
+}
+
+const birthYear = 2001;
+let century;
+if(birthYear<=2000){
+  century = 20;
+}else{
+  century =21;
+}
+console.log(century);
+
+
