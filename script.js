25917c8f6e0df6994b76657d7f3805a2a94edbe4
Ivan-Andr <levchenko-ivan@mail.ru>
2021-08-18 23:02:31 +0500

    Type conversation and coercion

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ console.log(Ivan);
 const ivanNew = `I'm ${firtstName},a ${birthYear} years old ${job}!`;
 console.log(ivanNew);
 console.log(ivanNew);*/
-const age=17;
+
+/*const age=17;
 //const isOldEnough=age>=18;
 if(age>=18) {
   console.log('Sarah can start driving license');
@@ -33,11 +34,25 @@ if(age>=18) {
 
 const birthYear = 2001;
 let century;
-if(birthYear<=2000){
+if(birthYear <= 2000){
   century = 20;
 }else{
   century =21;
 }
-console.log(century);
+console.log(century);*/
+
+const birthYear = '1987';
+console.log (Number(birthYear), birthYear);
+console.log (Number(birthYear) + 18);
+console.log (Number ("Ivan"));
+
+let n = '1';
+console.log (n + 1);
+console.log (n-1);
+console.log(2+3+4+'5');
+console.log('10'-'4'-'3'-'2'+'5');
+
+
+
 
 
