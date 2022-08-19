0ccf6ab6e5fe3570cc1685960edb882fcd63d1e4
Ivan-Andr <levchenko-ivan@mail.ru>
2022-08-19 21:42:18 +0500

    Selecting and manipulating elements

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,8 +1,16 @@
 'use strict';
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉Correct Number!';
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 27;
-console.log(document.querySelector('.guess').value);
+console.log(document.querySelector('.guess').value);*/
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '❗ No number';
+  }
+  console.log(guess, typeof guess);
+});
