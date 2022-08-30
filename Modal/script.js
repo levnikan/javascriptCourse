db1d7af711433bd9c51ba398366311884c181b33
Ivan-Andr <levchenko-ivan@mail.ru>
2022-08-30 22:10:14 +0500

    working with classes

--- Modal/script.js
+++ Modal/script.js
@@ -5,8 +5,24 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 //выбор всех объектов класса .show-modal
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+//console.log(btnsOpenModal);
 
+//функция открытия окна
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+//функция закрытия окна
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+//пробегаем циклом по всем элементам массива из кнопок
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+//закрытин окна нажатием на крестик
+btnCloseModal.addEventListener('click', closeModal);
+//закрытие окна кликом вне этого окна
+overlay.addEventListener('click', closeModal);
