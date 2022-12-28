0c7cca379867d16e038da98888b33106807e6a45
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2022-12-28 21:50:12 +0500

    destructing objects

--- ArraysModernOperators&Strings/script.js
+++ ArraysModernOperators&Strings/script.js
@@ -11,11 +11,78 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    adress,
+  }) {
+    console.log(`Order received! 
+    ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
+    will be delivered to ${adress} at ${time}`);
+  },
 };
 
+restaurant.orderDelivery({
+  time: '22:30',
+  adress: 'chambil2 - 3',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  adress: 'Nukus str., 2',
+  starterIndex: 1,
+});
+
+//Destructing objects
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, tags, hours);
+// default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//mutating variables
+let a = 111;
+let b = 1000;
+const obj = { a: 23, b: 25, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+//nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+/*
+///////////////////////////////
+//Destructing Arrays
 let [main, , secondary] = restaurant.categories;
 console.log(main, secondary);
 
@@ -36,8 +103,8 @@ const nested = [2, 4, [5, 6]];
 //const [i, , j] = nested;
 //console.log(i, j);
 const [i, , [j, k]] = nested;
-console.log(i, j, k);
 
+console.log(i, j, k);
 // default value
 const [p = 1, q = 1, r = 1] = [8];
-console.log(p, q, r);
+console.log(p, q, r); */
