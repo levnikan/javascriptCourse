896b9027ec51b2c78ad3e4188c42b43817f82607
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-01-02 17:36:40 +0500

    for-of looping && enhased objects literals

--- ArraysModernOperators&Strings/script.js
+++ ArraysModernOperators&Strings/script.js
@@ -5,31 +5,35 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const hours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0,
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0,
-      close: 24,
-    },
-  },
-
-  order: function (starterIndex, mainIndex) {
+  // property without function keyword - new syntax
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-
+  //ES6 enhased objects literals
+  hours,
   orderDelivery: function ({
     starterIndex = 1,
     mainIndex = 0,
@@ -48,7 +52,17 @@ const restaurant = {
     console.log(otherIngridients);
   },
 };
+/*
+// The FOR-OF loop
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of menu) {
+  console.log(item);
+}
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}:${el}`);
+}*/
 
+/*
 //////Logical Assignment operators
 
 const rest1 = {
@@ -59,6 +73,7 @@ const rest2 = {
   name: 'La Piazza',
   owner: 'Giovanni Rossi',
 };
+
 // OR assignment operator
 //rest2.numGuest = rest2.numGuest || 10;
 //rest1.numGuest = rest1.numGuest || 10;
@@ -75,6 +90,7 @@ rest2.owner &&= '<ANONYMOUS>';
 rest1.owner &&= '<ANONYMOUS>';
 console.log(rest1);
 console.log(rest2);
+*/
 /*
 ///////////////////
 //Nulish Coalesing operator
