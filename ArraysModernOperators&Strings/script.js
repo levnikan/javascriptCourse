1e9da8a7483719e992ad3afa96fa9a206008de39
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-01-03 22:38:47 +0500

    strings part 1

--- ArraysModernOperators&Strings/script.js
+++ ArraysModernOperators&Strings/script.js
@@ -52,6 +52,46 @@ const restaurant = {
     console.log(otherIngridients);
   },
 };
+/////working with the strings part 1
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+console.log(plane[0]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('You got the middle seat');
+  } else {
+    console.log('You got lucky!!!');
+  }
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+console.log(new String('ivan'));
+console.log(typeof new String('ivan'));
+console.log(typeof new String('ivan').slice(1));
+/////working with the strings part 2
+
+/*
 //////////Maps:fundamentals
 const rest = new Map();
 rest.set('name', 'Classico italiano');
@@ -113,6 +153,7 @@ console.log([...question]);
 console.log([question.entries()]);
 console.log([...question.keys()]);
 console.log([question.values()]);
+*/
 /*
 ///////////////////////SETS
 const orderSet = new Set([
