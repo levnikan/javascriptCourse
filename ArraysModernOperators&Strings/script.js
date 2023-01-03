4f1fc2b0f294c50b710619f8f912c887f83c7400
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-01-03 15:21:58 +0500

    Maps

--- ArraysModernOperators&Strings/script.js
+++ ArraysModernOperators&Strings/script.js
@@ -52,7 +52,69 @@ const restaurant = {
     console.log(otherIngridients);
   },
 };
-//SETS
+//////////Maps:fundamentals
+const rest = new Map();
+rest.set('name', 'Classico italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'we are open :D')
+  .set(false, 'we are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(2));
+const time = 7;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+console.log(rest.has('categories'));
+rest.delete(2);
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+//rest.clear();
+console.log(rest.size);
+console.log(rest.get(arr));
+
+////////Maps Iteration
+const question = new Map([
+  ['question', 'What is the best PL?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JS'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try Again'],
+]);
+console.log(question);
+// convert object to maps
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+let answer = Number(prompt('Your answer'));
+console.log(answer);
+if (answer === question.get('correct')) {
+  console.log(question.get(true));
+} else {
+  console.log(question.get(false));
+}
+console.log(question.get(question.get('correct') === answer));
+// convert map to array
+console.log([...question]);
+console.log([question.entries()]);
+console.log([...question.keys()]);
+console.log([question.values()]);
+/*
+///////////////////////SETS
 const orderSet = new Set([
   'Pasta',
   'Pizza',
@@ -78,7 +140,9 @@ console.log(staffUnique);
 console.log(
   new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
 );
+
 console.log(new Set('levchenkoivan').size);
+*/
 /*
 //////LOOPing objects
 //property names
