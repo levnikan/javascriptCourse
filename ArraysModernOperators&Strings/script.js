d33e5972a1c36eff9c0ff3d2c33b2812d74f3c29
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-01-12 15:56:12 +0500

    String transporming (web API data)- example

--- ArraysModernOperators&Strings/script.js
+++ ArraysModernOperators&Strings/script.js
@@ -5,6 +5,21 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+////result
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+const getCode = str => str.slice(0, 3).toUpperCase();
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(37);
+  console.log(output);
+}
+/*
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
 const openingHours = {
@@ -51,7 +66,7 @@ const restaurant = {
     console.log(mainIngridient);
     console.log(otherIngridients);
   },
-};
+};*/
 
 /*
 /////working with the strings part 1
@@ -92,7 +107,7 @@ console.log(new String('ivan'));
 console.log(typeof new String('ivan'));
 console.log(typeof new String('ivan').slice(1));
 */
-
+/*
 /////working with the strings part 2
 const airline = 'TAP Air Portugal';
 console.log(airline.toLowerCase());
@@ -178,6 +193,8 @@ const planesInLine = function (n) {
 };
 planesInLine(10);
 planesInLine(3);
+*/
+
 /*
 //////////Maps:fundamentals
 const rest = new Map();
