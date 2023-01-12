bf9ffccf10406fe01bf0af59afc4d0e442aff433
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-01-12 14:36:31 +0500

    Coding challenge #4

--- DataSructures-ModernOerators&Strings/script.js
+++ DataSructures-ModernOerators&Strings/script.js
@@ -145,7 +145,7 @@ for (const [team, odd] of entries) {
   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
   console.log(`Odd of ${teamStr}: ${odd}`);
 }*/
-
+/*
 // Bonus Task
 const scorers = {};
 for (let player of game.scored) {
@@ -156,7 +156,7 @@ for (let player of game.scored) {
   }
 }
 console.log(scorers);
-
+*/
 ///////////////////////////////////////
 // Coding Challenge #3
 
@@ -215,3 +215,72 @@ for (const [key, value] of gameEvents) {
     : console.log(`[SECOND HALF] ${key}: ${value}`);
 }
 */
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+///my code
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  //console.log(text);
+  const textArray = text.split('\n');
+  //console.log(textArray);
+  for (let i = 0; i < textArray.length; i++) {
+    let newStr = textArray[i].trim().toLowerCase();
+    //console.log(newStr.indexOf('_'));
+    let newSt1 =
+      newStr.slice(0, newStr.indexOf('_')) +
+      newStr[newStr.indexOf('_') + 1].toUpperCase() +
+      newStr.slice(newStr.indexOf('_') + 2);
+    console.log(newSt1.padEnd(20) + '✅'.repeat(i + 1));
+  }
+});
+
+//Jonas Schmedtmann's code
+//document.body.append(document.createElement('textarea'));
+//document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
