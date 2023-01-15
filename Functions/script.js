d644f637db3d660450ce998162b23f985ed840c7
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-01-15 15:47:15 +0500

    Higher order functions and functions trturning functions

--- Functions/script.js
+++ Functions/script.js
@@ -19,6 +19,7 @@ createBooking('LH123', 2, 800); // LH123, 2, 800
 createBooking('LH123', 5); // LH123, 5, 995
 createBooking('LH123', undefined, 5000); //numPassangers - will be default parametr
 */
+/*
 const flight = 'LH234';
 const ivan = {
   name: 'Levchenko Ivan',
@@ -42,3 +43,44 @@ const newPassport = function (person) {
 };
 newPassport(ivan);
 checkIn(flight, ivan);
+*/
+
+/*
+//Function accepting callback functions
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+//higher order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Trasformed by: ${fn.name}`);
+};
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
+
+//JS uses callbacks all the time
+const high5 = function () {
+  console.log('👍');
+};
+document.body.addEventListener('click', high5);
+['Ivan', 'Tatyana', 'Mariya'].forEach(high5);
+*/
+// functions returning functions
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+// same with arrow function
+const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
+
+const greeterHey = greet('Hey');
+greeterHey('Ivan');
+greeterHey('Koshka');
+greet('Hello')('Ivan');
+greetArrow('Hi')('Ivan');
