5dac38cc2f18c9ca782f9e62d2f2ede1919f262d
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-01-23 17:43:31 +0500

    Simple array methods

--- Arrays/script.js
+++ Arrays/script.js
@@ -74,3 +74,42 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+let arr = ['a', 'b', 'c', 'd', 'e'];
+//SLICE - doesn't change original array
+console.log(arr.slice(2));
+console.log(arr.slice(2, -1));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+//SPLICE changes original array (mutate)
+//console.log(arr.splice(3));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE changes original array (mutate)
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//JOIN
+console.log(letters.join('_'));
+
+//AT
+const arr3 = [23, 11, 65];
+console.log(arr3[2]);
+console.log(arr3.at(0));
+// getting last element of array
+console.log(arr3[arr3.length - 1]);
+console.log(arr3.slice(-1)[0]);
+console.log(arr.at(-1));
