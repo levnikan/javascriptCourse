cf330ff27e50f37a9a8e127325caeb8efd9893f8
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-01-26 10:00:13 +0500

    max value of array with reduce()

--- Arrays/script.js
+++ Arrays/script.js
@@ -202,3 +202,13 @@ console.log(balance);
 let balance2 = 0;
 for (const mov of movements) balance2 += mov;
 console.log(balance2);
+
+//maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) {
+    return acc;
+  } else {
+    return mov;
+  }
+}, movements[0]);
+console.log(max);
