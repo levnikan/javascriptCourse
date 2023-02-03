6c5007993a21ddc2229bed306f756ba58bc78cf1
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-02-03 15:23:47 +0500

    implementing loan request

--- Arrays/script.js
+++ Arrays/script.js
@@ -162,6 +162,18 @@ btnTransfer.addEventListener('click', function (e) {
     updateUi(currentAccount);
   }
 });
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    //add movement
+    currentAccount.movements.push(amount);
+    //update ui
+    updateUi(currentAccount);
+    //clear the input field
+    inputLoanAmount.value = '';
+  }
+});
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
   //console.log('Delete');
